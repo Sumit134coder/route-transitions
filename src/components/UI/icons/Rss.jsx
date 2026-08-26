02ba09d37b80fd9fa8fbeea24b392dd0c2cdcc7a
sumit_mehra <mehrasumit356@gmail.com>
2026-08-26 15:26:50 +0530

    add : footer icons

---
+++ src/components/UI/icons/Rss.jsx
@@ -0,0 +1,21 @@
+const Rss = ({className}) => {
+  return (
+    <svg
+        width="15"
+        height="15"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className={className}
+      >
+        <path d="M4 11a9 9 0 0 1 9 9" />
+        <path d="M4 4a16 16 0 0 1 16 16" />
+        <circle cx="5" cy="19" r="1" />
+      </svg>
+  )
+}
+
+export default Rss
---
+++ src/components/UI/icons/index.js
@@ -0,0 +1,9 @@
+import Instagram from "./Instagram";
+import Pinterest from "./Pinterest";
+import X from "./X";
+import Rss from "./Rss";
+
+
+export {
+    Instagram , Pinterest , X ,Rss
+}
