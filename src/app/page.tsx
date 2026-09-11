0ca15351c997be6d7866ab48755faceafc3b346c
sumit_mehra <mehrasumit356@gmail.com>
2026-09-11 15:20:29 +0530

    update :Card component and transition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
 import { PHOTOGRAPHERS } from "@/lib/constants/footer";
 import { galleryData } from "@/lib/constants/data";
 import SortOptions from "@/components/UI/SortOptions";
 import PhotographerFilter from "@/components/UI/PhotographerFilter";
-import Link from "next/link";
-import { ViewTransition } from "react";
+import GalleryImageCard from "@/components/UI/GalleryImageCard";
 
 export default function Home() {
   return (
@@ -22,39 +20,7 @@ export default function Home() {
       <div className="max-w-6xl mx-auto px-6 py-8">
         <div className="columns-1 sm:columns-2 lg:columns-3 gap-4 space-y-4">
           {galleryData.map((photo, i) => (
-            <div key={photo.imgSrc} className="break-inside-avoid">
-              <Link
-                href={`/photos/${i}`}
-                className="group w-full text-left block"
-              >
-                <ViewTransition name={`photo-${i}`}>
-                  <div className="relative overflow-hidden rounded-lg">
-                    <Image
-                      src={photo.imgSrc}
-                      alt={photo.title}
-                      width={400}
-                      height={300}
-                      className="w-full h-auto object-cover rounded-lg"
-                    />
-                    {/* Hover overlay */}
-                    <div className="absolute inset-0 bg-black/0 group-hover:bg-black/20 transition-all duration-300" />
-                    <div className="absolute bottom-0 left-0 right-0 p-4 translate-y-2 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-300">
-                      <div className="text-white text-sm font-medium drop-shadow">
-                        {photo.place}
-                      </div>
-                      <div className="mt-2 px-1">
-                        <p className="font-serif text-stone-900 font-semibold text-sm leading-tight">
-                          {photo.title}
-                        </p>
-                        <p className="text-stone-400 text-xs mt-0.5">
-                          {photo.photographer} · {photo.date}
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </ViewTransition>
-              </Link>
-            </div>
+            <GalleryImageCard galleryImage={photo} path={i} key={i} />
           ))}
         </div>
 
--- src/components/UI/GalleryImageCard.tsx
+++ src/components/UI/GalleryImageCard.tsx
@@ -2,30 +2,30 @@ import Image from "next/image";
 import Link from "next/link";
 import { ViewTransition } from "react";
 
-const GalleryImageCard = ({ galleryImage }: any) => {
+const GalleryImageCard = ({ galleryImage , path }: any) => {
   return (
     <div key={galleryImage.imgSrc} className="break-inside-avoid">
-      <Link href={`/photos`} className="group w-full text-left block">
-        <ViewTransition name={`photo-`}>
+      <Link href={`/photos/${path}`} className="group w-full text-left block">
+        <ViewTransition name={`photo-${path}`}>
           <div className="relative overflow-hidden rounded-lg">
             <Image
               src={galleryImage.imgSrc}
               alt={galleryImage.title}
               width={400}
               height={300}
-              className="w-full h-auto object-cover rounded-lg"
+              className="w-full h-auto object-cover rounded-lg group-hover:scale-120 transition-transform duration-300"
             />
             {/* Hover overlay */}
-            <div className="absolute inset-0 bg-black/0 group-hover:bg-black/20 transition-all duration-300" />
+            <div className="absolute inset-0 group-hover:bg-linear-to-t from-foreground/50 to-foreground/10 transition-all duration-300" />
             <div className="absolute bottom-0 left-0 right-0 p-4 translate-y-2 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-300">
-              <div className="text-white text-sm font-medium drop-shadow">
+              <div className="text-background text-sm font-medium drop-shadow">
                 {galleryImage.place}
               </div>
-              <div className="mt-2 px-1">
-                <p className="font-serif text-stone-900 font-semibold text-sm leading-tight">
+              <div className="mt-2">
+                <p className="text-background font-semibold text-sm leading-tight">
                   {galleryImage.title}
                 </p>
-                <p className="text-stone-400 text-xs mt-0.5">
+                <p className="text-background text-xs mt-0.5">
                   {galleryImage.photographer} · {galleryImage.date}
                 </p>
               </div>
